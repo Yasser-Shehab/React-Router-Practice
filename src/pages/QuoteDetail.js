86eeebcffe8665c5adc9040f195e84945f19c7c6
AquaApple <yassershehab9@gmail.com>
2022-09-09 00:41:09 +0200

    added nested route

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 
 function QuoteDetail() {
   const params = useParams();
 
-  return <h1>Quote Detail {params.quoteId}</h1>;
+  return (
+    <>
+      <h1>Quote Detail</h1>
+      <p>{params.quoteId}</p>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </>
+  );
 }
 
 export default QuoteDetail;
