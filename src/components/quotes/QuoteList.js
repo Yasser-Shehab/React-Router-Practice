e7d114d643299624e6a4727c0f936ea0815ef81e
AquaApple <yassershehab9@gmail.com>
2022-09-09 11:34:10 +0200

    Added Button to change Query Params

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,19 +1,23 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
+import { useHistory } from "react-router-dom";
 
-import QuoteItem from './QuoteItem';
-import classes from './QuoteList.module.css';
+import QuoteItem from "./QuoteItem";
+import classes from "./QuoteList.module.css";
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const changeSortHandler = () => {
+    history.push("/quotes?sort=" + "asc");
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortHandler}>Sort Ascending</button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
-          <QuoteItem
-            key={quote.id}
-            id={quote.id}
-            author={quote.author}
-            text={quote.text}
-          />
+          <QuoteItem key={quote.id} id={quote.id} author={quote.author} text={quote.text} />
         ))}
       </ul>
     </Fragment>
